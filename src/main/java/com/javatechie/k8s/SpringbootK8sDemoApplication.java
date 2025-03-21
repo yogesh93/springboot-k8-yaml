0db8cba944a1c9fd469d9cdc8ea949aa5a67711a
Yogesh Sharma <yogesh.sharma@birdeye.com>
2025-03-21 12:53:28 +0530

    ys-test-branch changes done

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -23,6 +23,7 @@ public class SpringbootK8sDemoApplication {
 	}
 	@GetMapping("/message2")
 	public String DisplayCOde1234() {
+
 		String a = "Congratulation you successfully deployed your application to kubernetes !!";
 		String a1 = "Congratulation you successfully deployed your application to kubernetes !!";
 		String a32 = "Congratulation you successfully deployed your application to kubernetes !!";
