cf021c55e76855c92f340240bee23a01e561a1c0
Yogesh Sharma <yogesh.sharma@birdeye.com>
2025-03-25 16:52:19 +0530

    ys-test-branch-2

--- src/main/java/com/javatechie/k8s/DBConnector.java
+++ src/main/java/com/javatechie/k8s/DBConnector.java
@@ -1,12 +1,12 @@
-//package com.javatechie.k8s;
-//
-//public class DBConnector {
-//
-//    public static void main(String[] args) {
-//        String username = "admin";
-//        String password = "password123";
-//    }
-//
-//}
+package com.javatechie.k8s;
+
+public class DBConnector {
+
+    public static void main(String[] args) {
+        String username = "admin";
+        String password = "password123";
+    }
+
+}
 
 
--- src/main/java/com/javatechie/k8s/NullPointerExample.java
+++ src/main/java/com/javatechie/k8s/NullPointerExample.java
@@ -1,10 +1,10 @@
-//package com.javatechie.k8s;
-//
-//public class NullPointerExample {
-//
-//        public static void main(String[] args) {
-//            String name = null;
-//            System.out.println(name.length());
-//        }
-//
-//}
+package com.javatechie.k8s;
+
+public class NullPointerExample {
+
+        public static void main(String[] args) {
+            String name = null;
+            System.out.println(name.length());
+        }
+
+}
