f5d0ded41beefa572e2ca0e549babc84a47a2b47
Yogesh Sharma <yogesh.sharma@birdeye.com>
2025-03-25 12:15:20 +0530

    ys-test-branch-2 changes done

--- src/main/java/com/javatechie/k8s/DBConnector.java
+++ src/main/java/com/javatechie/k8s/DBConnector.java
@@ -1,10 +1,10 @@
-package com.javatechie.k8s;
-
-public class DBConnector {
-
-    public static void main(String[] args) {
-        String username = "admin";
-        String password = "password123";
-    }
-
-}
+//package com.javatechie.k8s;
+//
+//public class DBConnector {
+//
+//    public static void main(String[] args) {
+//        String username = "admin";
+//        String password = "password123";
+//    }
+//
+//}
